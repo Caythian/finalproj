fc5c9119b51d8ea000683e7666e0fbf690722769
Caythian <xcatherine020202@gmail.com>
2023-11-21 23:22:49 +0100

    change in submenu(unsuccessful)

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -17,20 +17,24 @@ function Menu() {
         <li>
           <Link to='/gamedesign'>Game Design</Link>
           <ul className="sub-menu">
-            <li><Link to='/about/history'>History</Link></li>
-            <li><Link to='/about/team'>Team</Link></li>
+            <li><Link to='/gamedesign/history'>History</Link></li>
+            <li><Link to='/gamedesign/team'>Team</Link></li>
           </ul>
         </li>
         <li>
           <Link to='/escapism'>Escapism</Link>
           <ul className="sub-menu">
-            {/* Sub-menu items */}
+            <li><Link to='/escapism/history'>History</Link></li>
+            <li><Link to='/escapism/team'>Team</Link></li>
+            <li><Link to='/escapism/about'>About</Link></li>
           </ul>
         </li>
         <li>
           <Link to='/more'>More</Link>
           <ul className="sub-menu">
-            {/* Sub-menu items */}
+            <li><Link to='/more/history'>History</Link></li>
+            <li><Link to='/more/team'>Team</Link></li>
+            <li><Link to='/more/about'>More</Link></li>
           </ul>
         </li>
       </ul>
