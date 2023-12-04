12cec115ff897c306ddf18d725381d0552fdb2cf
Caythian <xcatherine020202@gmail.com>
2023-12-04 20:54:15 +0100

    home page done

--- src/Components/Column.js
+++ src/Components/Column.js
@@ -2,7 +2,10 @@ import React from 'react';
 import '../App.css';
 import '../Components/Column.css';
 import { Link } from 'react-router-dom';
+import totkimg2 from '../Images/totkimg2.jpeg';
+import totkimg3 from '../Images/totkimg3.jpeg';
 import totkimg4 from '../Images/totkimg4.jpeg';
+import totkimg8 from '../Images/totkimg8.jpeg';
 
 function Column() {
   return (
@@ -10,27 +13,27 @@ function Column() {
       <div className="row">
         <div className="column">
           <h3>Realism</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet rutrum mauris, non malesuada risus. Donec tempor nibh ac facilisis facilisis.</p>
-          <img src={totkimg4} alt="Kakoriko Village"></img>
-          <Link to="/realism" className="button">Click Here</Link> {/* Modify the path as needed */}
+          <p>Learn how the game developers create a sense of reality in environmental design. </p>          
+          <img src={totkimg8} alt="on horseback"></img>
+          <Link to="/realism" className="button">Click Here</Link> 
         </div>
         <div className="column">
           <h3>Fantasy</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet rutrum mauris, non malesuada risus. Donec tempor nibh ac facilisis facilisis.</p>
-          <img src={totkimg4} alt="Kakoriko Village"></img>
-          <Link to="/fantasy" className="button">Click Here</Link> {/* Modify the path as needed */}
+          <p>Learn how the color choice in design contributes to the creation of a fantasy and how it impacts the player.</p>
+          <img src={totkimg3} alt="sky islands"></img>
+          <Link to="/fantasy" className="button">Click Here</Link> 
         </div>
         <div className="column">
           <h3>Escapism</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet rutrum mauris, non malesuada risus. Donec tempor nibh ac facilisis facilisis.</p>
+          <p>Explore the notion of escapism in gaming, and the potential of utilizing the immersive experience in gaming. </p>
           <img src={totkimg4} alt="Kakoriko Village"></img>
-          <Link to="/escapism" className="button">Click Here</Link> {/* Modify the path as needed */}
+          <Link to="/escapism" className="button">Click Here</Link> 
         </div>
         <div className="column">
           <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet rutrum mauris, non malesuada risus. Donec tempor nibh ac facilisis facilisis.</p>
-          <img src={totkimg4} alt="Kakoriko Village"></img>
-          <Link to="/about" className="button">Click Here</Link> {/* Modify the path as needed */}
+          <p>Learn about why the website exists and give feedback. </p>
+          <img src={totkimg2} alt="cliff sunset"></img>
+          <Link to="/about" className="button">Click Here</Link> 
         </div>
       </div>
     </div>
