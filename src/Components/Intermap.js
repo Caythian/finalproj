a1e1703d866fa3216724af46343b7298f17d956b
Caythian <xcatherine020202@gmail.com>
2023-12-09 22:57:43 +0100

    Gamedesign map comp

---
+++ src/Components/Intermap.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import { useState } from 'react';
+import '../App.css';
+import './Map.css';
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import Totkmap from '../Images/Totkmap.png';
+//import images below:
+import totkimg11 from '../Images/totkimg11.jpg';
+import mongo from '../Images/Mongolia.jpeg';
+import totkimg12 from '../Images/totkimg12.jpg';
+import bryce from '../Images/Bryce.jpg';
+import totkimg13 from '../Images/totkimg13.jpg';
+import baobab from '../Images/Baobab.jpg';
+import totkimg14 from '../Images/totkimg14.jpg';
+import tree from '../Images/tree.jpeg';
+import totkimg15 from '../Images/totkimg15.jpeg';
+import canyon from '../Images/canyon.jpeg';
+
+function Map(){
+    delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    
+});
+const imageBounds = [[0, 0], [100, 200]]; // Use the calculated boundsWidth here
+const center = [(imageBounds[0][0] + imageBounds[1][0]) / 2, (imageBounds[0][1] + imageBounds[1][1]) / 2];
+const [selectedLocation, setSelectedLocation] = useState(null);
+
+const locations = {
+    hyrulefield: {
+        name: 'Hyrule Field',
+        image1: totkimg11, 
+        image2: mongo,
+        realloc:'Temporate Grasslands',
+        description: 'The climate and terrain here is similar to the temporate grasslands, such as the Mongolian-Manchurian steppe crossing over China, Mongolia, and Russia.'
+    },
+    gerudo:{
+        name:'Gerudo Highlands',
+        image1:totkimg12,
+        image2:bryce,
+        realloc:'Bryce National Park, UT, USA',
+        description:'The terrain of Gerudo Highlands looks very similar to the terrain of Bryce National Park in Utah, the US.',
+    },
+    taobab:{
+        name:'Taobab Grasslands',
+        image1:totkimg13,
+        image2:baobab,
+        realloc:'Baobab trees in Madagascar',
+        description:'The trees on taobab Grasslands are inspired by Baobob trees in Madagascar.',
+    },
+    seres:{
+        name:'Seres Scablands',
+        image1:totkimg14,
+        image2:tree,
+        realloc:'Dragon blood trees in Yemen',
+        description:'Those "mushroom" trees scattered in Seres Scablands resemble the dragon blood trees on the islands of Socotra, in Yemen.',
+    },
+    canyon:{
+        name:'Karusa Valley',
+        image1:totkimg15,
+        image2:canyon,
+        realloc:'Antelope Canyon in AZ, US',
+        description:'The vally deep in the Gerudo region resembles the Antelope Canyon in Arizona, US.',
+    },
+        // gerudo:{
+    //     name:'',
+    //     image1:'',
+    //     image2:'',
+    //     realloc:'',
+    //     description:'',
+    // },
+}
+const handleMarkerClick = (locationKey) => {
+    if (selectedLocation && selectedLocation.name === locations[locationKey].name) {
+        // If the current location is already selected, set it to null to hide the details
+        setSelectedLocation(null);
+    } else {
+        // Otherwise, set the selected location to the one that was clicked
+        setSelectedLocation(locations[locationKey]);
+    }
+};
+
+const hyrulefield=[65,90];
+const gerudo=[50,50];
+const karusavalley=[50,18];
+const faronforest=[30,120]
+const taobab=[35,78];
+const akkla=[83,173];
+const spiral=[80,190];
+const Kakariko=[58,138];
+const seres=[71,52];
+const Kara=[36,38];
+
+
+return(
+<div className="mapanddes" id="map">
+<MapContainer center={hyrulefield} zoom={2.5} scrollWheelZoom={false}>
+    <ImageOverlay url={Totkmap} bounds={imageBounds} />
+    <Marker position={hyrulefield} eventHandlers={{ click: () => handleMarkerClick('hyrulefield') }}>
+        <Popup>
+            Hyrule Field
+        </Popup>
+    </Marker>
+    <Marker position={gerudo} eventHandlers={{ click: () => handleMarkerClick('gerudo') }}>
+        <Popup>
+            Gerudo Highlands
+        </Popup>
+    </Marker>
+    <Marker position={taobab} eventHandlers={{ click: () => handleMarkerClick('taobab') }}>
+        <Popup>
+           Taobab Grassland
+        </Popup>
+    </Marker>
+    <Marker position={faronforest}>
+        <Popup>
+            Faron Rainforest
+        </Popup>
+    </Marker>
+    <Marker position={karusavalley} eventHandlers={{ click: () => handleMarkerClick('canyon') }}>
+        <Popup>
+            Karusa Valley
+        </Popup>
+    </Marker>
+    <Marker position={akkla}>
+        <Popup>
+            North Akkala
+        </Popup>
+    </Marker>
+    <Marker position={spiral}>
+        <Popup>
+            Rist Peninsula
+        </Popup>
+    </Marker>
+    <Marker position={Kakariko}>
+        <Popup>
+            Kakariko Village
+        </Popup>
+    </Marker>
+    <Marker position={seres} eventHandlers={{ click: () => handleMarkerClick('seres') }}>
+        <Popup>
+            Seres Scablands
+        </Popup>
+    </Marker>
+    <Marker position={Kara}>
+        <Popup>
+           Kara Kara Bazaar
+        </Popup>
+    </Marker>
+</MapContainer>
+<div className="details-container">
+    {selectedLocation ? (
+        <>
+            <img src={selectedLocation.image1} alt={selectedLocation.name} />
+            <img src={selectedLocation.image2} alt={selectedLocation.realloc}/>
+            <div className="description">{selectedLocation.name}</div>
+            <div className='descriptionsmol'>{selectedLocation.description}</div>
+        </>
+    ) : (
+        <div className="placeholder">Select a location</div>
+    )}
+</div>
+</div>
+);}
+
+export default Map;
--- src/Gamedesign.js
+++ src/Gamedesign.js
@@ -2,12 +2,13 @@ import './App.css';
 import React,{useState,useEffect} from 'react';
 import Menu from './Components/Menu';
 import Footer from './Components/Footer';
-import './App.css';
 import {Placeholder, Divider} from './Components/Format';
 import { Routes, Route } from 'react-router-dom';
 import ImageSection from './Components/Imagereveal';
 import Column from './Components/Column';
+import Map from './Components/Intermap';
 //import image below
+import totkimg10 from './Images/totkimg10.jpg';
 
 function Gamedesign(){
     return(
@@ -15,9 +16,26 @@ function Gamedesign(){
             <Menu/>
             <Placeholder/>
             <div className="textholder">
-                
+                <div className="title">Immersive Game Design</div>
+                <div className="body">
+                     A harmonious blend of realistic and fantastical elements is key to achieving immersion in gaming. Incorporating realistic aspects such as advanced physics, high-quality graphics, and well-developed characters enhances a player's connection to the game world, thereby making the experience more engaging and relatable. Conversely, fantasy elements offer players a chance to explore beyond the realms of normalcy, ranging from enchanting worlds and mythical creatures to extraordinary abilities and scenarios. Fantasy serves as an escape from the mundane, inviting players into experiences unattainable in real life, thus sparking creativity and adding excitement and intrigue to the gameplay. The subsequent section will delve into the visual components that amalgamate realism with fantasy, albeit without an extensive focus on the finer points of game design.                </div>
+                <ImageSection backgroundImage={totkimg10} alt="Link confronting a silver boko"></ImageSection>
+                <div className="body">
+                "Zelda: Tears of the Kingdom" stands as a quintessential example of immersive gaming. Its expansive open world and captivating landscapes showcase a blend of realistic elements paired with stylized visuals, exemplifying this balance.
+                </div>
+                <Divider/>
+                <div className="subtitle"> Biomes Inspired by Real-life environments. </div>
+                <div className="body">
+                To enhance the intrigue and variety in "Zelda: Tears of the Kingdom," the game's map features a diverse array of biomes, each inspired by real-world climates. This approach adds a layer of realism to the fantasy setting, allowing players to traverse through distinct environments that mimic actual geographical conditions. Moreover, the game includes specific locations that reference real-world places, adding depth to the game's world and also creating a sense of familiarity and realism within the fantastical universe of "Zelda".
+                </div>
+                <div className="body">
+                    You can click on the locations on the map below to see the different biomes.
+                </div>
+                <Map/>
+                <Divider/>
+                <div className="subtitle"></div>
             </div>
-            <Footer></Footer>
+            <Footer/>
         </div>
     )
 }
