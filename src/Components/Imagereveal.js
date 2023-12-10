19a7c2d233a334cdca5ea8b727e24d047fc97593
Caythian <xcatherine020202@gmail.com>
2023-12-10 13:36:26 +0100

    Imagesection Fully Mobile

--- src/Components/Imagereveal.js
+++ src/Components/Imagereveal.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import './ImageSection.css'; // Import the CSS file
+import React, { useState, useEffect } from 'react';
+import './ImageSection.css';
 
 function ImageSection({ backgroundImage, children }) {
-    return (
-        <div 
-            className="imagesection" 
-            id="image-reveal-section" 
-            style={{ background: `url(${backgroundImage}) no-repeat center center/cover fixed` }}>
-            {children}
-        </div>
-    );
+  const [isVisible, setIsVisible] = useState(true); // Default to true
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mobile = window.innerWidth < 600;
+      setIsMobile(mobile);
+
+      // If not mobile, always show children
+      if (!mobile) {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleVisibility = () => {
+    if (isMobile) {
+      setIsVisible(!isVisible);
+    }
+  };
+
+  return (
+      <div 
+          className="imagesection" 
+          id="image-reveal-section"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+          onClick={toggleVisibility}>
+          {isVisible && children}
+      </div>
+  );
 }
 
 export default ImageSection;
